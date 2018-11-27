def96ea292475a7be227ed08015e04f757adebc4
lq <1429110363@qq.com>
2018-11-27 11:43:53 +0800

    11

--- group-llx-master/Blog-Server/src/com/yc/dao/Test.java
+++
@@ -1,9 +0,0 @@
-package com.yc.dao;
-
-public class Test {
-	public static void main(String[] args) {
-		DBHelper helper = new DBHelper();
-		System.out.println("1");
-		System.out.println(helper.find("select * from user", null));
-	}
-}
